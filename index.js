128095b90804f1705f4900d4fd9198971fd77890
muhammed-jamiu <anatemuhammed@gmail.com>
2025-09-27 14:11:41 +0100

    bootstrap folder added

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ document.querySelector("#my-home").classList.add("active");
 // Attach click event to each nav item
 navItems.forEach((item) => {
   item.addEventListener("click", () => {
+    console.log("item-section", item);
     // Remove active class from all
-    navItems.forEach((nav) => nav.classList.remove("active"));
+    navItems.forEach((nav) => {
+      nav.classList.remove("active");
+      console.log("Nav-section", nav);
+    });
     // Add active class to clicked one
     item.classList.add("active");
   });
